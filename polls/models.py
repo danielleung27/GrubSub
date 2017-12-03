44bf6b1cda566778575e76d9dbc3d712bb6e574e
azou1103 <azou@princeton.edu>
2017-12-03 03:20:42 -0500

    appearance of directory, result pages

--- polls/models.py
+++ polls/models.py
@@ -4,26 +4,6 @@ from __future__ import unicode_literals
 from django.db import models
 #from django.utils.encoding import python_2_unicode_compatible
 
-# django model for food listings
-<<<<<<< HEAD
-# class Listing(models.Model):
-#     id = models.IntegerField()
-#     food_group = models.CharField(max_length=200)
-#     long_descr = models.CharField(max_length=200)
-#     short_descr = models.CharField(max_length=200)
-#     water = models.FloatField()
-#     energy = models.IntegerField()
-#     protein = models.FloatField()
-#     trans_fat = models.FloatField()
-#     carbohydrate = models.FloatField()
-#     fiber = models.FloatField()
-#     sugar = models.FloatField()
-#     ca = models.FloatField()
-#     sat_fat = models.FloatField()
-#     cholesterol = models.FloatField()
-#     vitamin_b = models.FloatField()
-#     na = models.FloatField()
-=======
 class Listing(models.Model):
     id_num = models.IntegerField()
     food_group = models.CharField(max_length=200)
@@ -41,4 +21,4 @@ class Listing(models.Model):
     cholesterol = models.FloatField()
     vitamin_b = models.FloatField()
     na = models.FloatField()
->>>>>>> refs/remotes/origin/master
+
