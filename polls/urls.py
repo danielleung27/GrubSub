1826003f9ba7d411a534539eb383e9205d8e5033
danielleung27 <danielleung27@yahoo.com>
2017-12-02 19:57:43 -0500

    models listings and new files added

--- polls/urls.py
+++ polls/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'^index$', views.index, name='index'),
+    url(r'^directory$', views.directory, name='directory'),
+	url(r'^searchresult$', views.searchresult, name='searchresult'),
 	url(r'^$', views.search, name='search'),
     url(r'results/$', views.results, name='results'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -12,6 +12,14 @@ def search(request):
 	}
 	return render(request, 'polls/search_form.html', context)
 
+def index(request):
+    return render(request, 'polls/index.html')
+
+def directory(request):
+	return render(request, 'polls/directory.html')
+
+def searchresult(request):
+	return render(request, 'polls/searchresult.html')
 
 def results(request):
     context = {
