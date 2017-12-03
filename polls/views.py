166f261c83f5b03914fb3ba31f2cf8dca4f09dc5
danielleung27 <danielleung27@yahoo.com>
2017-12-02 21:28:06 -0500

    randomized weights, added test file, removed imports on models

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Question
 from .forms import SearchForm
-from .foother import FoodSearch
 
 def search(request):
 	form = SearchForm()
